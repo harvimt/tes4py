2c2903713975ec4894bb3d3e2f988d3d2a69f640
Mark Harviston <mark.harviston@gmail.com>
2015-04-19 21:37:28 -0700

    more comprehensive test

--- tests/test_espesmformat.py
+++ tests/test_espesmformat.py
@@ -9,7 +9,9 @@ def test_accept1():
     esm = EspEsmFormat('E:/HDSteamLib/steamapps/common/Oblivion/data/Oblivion.esm')
     with esm:
         assert esm.header.flags.isesm
-        clot_r = next(esm['CLOT'].records)
+        clot_r = next(iter(esm['CLOT'].records))
+        for g in esm.groups:
+            [r for r in g.records]
         idata = clot_r['DATA'].item_data
         assert idata.gold_value == 8
         assert idata.weight == 4.0
